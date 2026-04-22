433a73a4ce88bb307b525c3251da0fd094e0bf4f
Lawrence Greco <emailul_tau@exemplu.com>
2026-04-23 00:05:12 +0300

    Prima modificare design, font titluri, umbre, paralax

--- head.php
+++ head.php
@@ -194,7 +194,7 @@ function render_head_assets()
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap" rel="stylesheet">
 
 <!-- Custom CSS -->
 <link rel="stylesheet" href="<?php echo htmlspecialchars(asset_url('style.css')); ?>?v=<?php echo filemtime(__DIR__ . '/style.css'); ?>">
